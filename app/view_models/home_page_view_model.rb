969fcacc73420b062fb93634debf17e7ae486796
pineapple69 <masterofpudding@hotmail.com>
2018-11-12 12:41:45 +0100

    added fake data base, featured events display properly, added host authentication

--- app/view_models/home_page_view_model.rb
+++ app/view_models/home_page_view_model.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class HomePageViewModel
   def featured_events
     Event.last(3).reverse
   end
 
   def closest_events
-    Event.order(:date).last(3).reverse
+    Event.order(:date).first(3).reverse
   end
 end
