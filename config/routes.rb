f0fecd3f3624197b2c5909c52229fcbdf13d27fa
zvrbsky <p-zarebski@wp.pl>
2018-12-09 20:57:34 +0100

    Email validation during registaration repaired in User view and added to Host view

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     get 'users/email_exists', to: 'users#email_exists'
+    get 'hosts/email_exists', to: 'hosts#email_exists'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
